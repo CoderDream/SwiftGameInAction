592a23d3061caed1fbbcd33ba81e8032c461127c
CoderDream <coderdream@gmail.com>
2019-04-03 22:23:22 +0800

    0710 20190403

---
+++ Chapter07/0708/SpaceInvaders/SpaceInvaders/EnemyBullet.swift
@@ -0,0 +1,55 @@
+//
+//  EnemyBullet.swift
+//  SpaceInvaders
+//
+//  Created by CoderDream on 2019/3/26.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EnemyBullet: NSObject {
+    var bombRect: CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
+    var bombView: UIImageView = UIImageView()
+    var bombTimer: Timer? = nil
+    var gameView:UIView = UIView()
+    
+    func  fireBullet(gameView: UIView, enemyList: Array<UIImageView>){
+        let randEnemy: Int = Int(arc4random()) % enemyList.count
+        let enemyView: AnyObject = enemyList[randEnemy] as AnyObject
+        self.gameView = gameView
+        let bombImage1: UIImage = UIImage(named: "bullet.png")!
+        let bombStartX = enemyView.frame.origin.x
+        
+        let imageRef1 = UIImageView(image: bombImage1)
+        imageRef1.frame = CGRect(x: 0, y: 0, width: 32, height: 64)
+        let imageRef2 = UIImageView(image: bombImage1)
+        imageRef2.frame = CGRect(x: 33, y: 0, width: 32, height: 64)
+        
+        let bombArray: NSMutableArray = NSMutableArray()
+        bombArray.add(imageRef1.image!)
+        bombArray.add(imageRef2.image!)
+        
+        self.bombRect = CGRect(x: bombStartX, y: enemyView.frame.origin.y, width: 8, height: 16)
+        self.bombView.image = imageRef2.image
+        
+        self.bombView.animationImages = bombArray as? [UIImage]
+        self.bombView.animationDuration = 3
+        self.gameView.addSubview(self.bombView)
+        self.bombView.startAnimating()
+        
+        // 移动子弹
+        bombTimer = Timer.scheduledTimer(timeInterval: 0.03, target: self, selector: #selector(moveBomb), userInfo: nil, repeats: true)
+    }
+    @objc func moveBomb() {
+        self.bombRect = self.bombRect.offsetBy(dx: 0, dy: 5)
+        self.bombView.frame = self.bombRect
+        if self.bombRect.origin.y > 440 {
+            self.bombTimer?.invalidate()
+            self.bombTimer = nil
+            self.bombView.removeFromSuperview()
+        }
+    }
+}
---
+++ Chapter07/0709/SpaceInvaders/SpaceInvaders/PlayerObject.swift
@@ -0,0 +1,63 @@
+//
+//  PlayerObject.swift
+//  SpaceInvaders
+//
+//  Created by CoderDream on 2019/3/26.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+class PlayerObject: NSObject {
+    var gameView: UIView = UIView()
+    var playerRect: CGRect = CGRect()
+    var playerImage: UIImage = UIImage(named: "ship.png")!
+    var playerView: UIImageView = UIImageView()
+    
+    var moveTimer: Timer? = Timer()
+    
+    var playerBullet = PlayerBullet()
+    
+    func fireBullet() {
+        playerBullet.fireBullet(gameView: self.gameView, playerView: self.playerView)
+    }
+    
+    func initPlayer(gameView: UIView) -> PlayerObject {
+        self.gameView = gameView
+        // 设置图像视图显示的图像
+        self.playerView.image = self.playerImage
+        self.playerRect = CGRect(x: 210, y: 400, width: 32, height: 32)
+        // 设置图像视图的框架
+        self.playerView.frame = self.playerRect
+        // 添加图像视图
+        self.gameView.addSubview(self.playerView)
+        return self
+    }
+    
+    func getPlayerView() -> UIView{
+        return self.playerView
+    }
+    
+    // 向右移动
+    @objc func movePlayerRight() {
+        //print("movePlayerRight")
+        let screenRect: CGRect = UIScreen.main.bounds
+        // 判断飞船的 x 的位置是否超出边界
+        if self.playerRect.origin.x <= screenRect.size.width - self.playerRect.size.width - 10 {
+            // 向右平移 3 像素
+            self.playerRect = self.playerRect.offsetBy(dx: 3, dy: 0)
+            self.playerView.frame = self.playerRect
+        }
+    }
+    
+    // 向左移动
+    @objc func movePlayerLeft() {
+        // 判断飞船的 x 的位置是否超出边界
+        if self.playerRect.origin.x >= 10 {
+            // 向左平移 3 像素
+            self.playerRect = self.playerRect.offsetBy(dx: -3, dy: 0)
+            self.playerView.frame = self.playerRect
+        }
+    }
+}
